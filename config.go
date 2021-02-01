9606a6645a5b77e2d2b5333bc1f95287bb0012d3
Brendan Schlaman <brendan.schlaman@gmail.com>
2021-02-01 16:43:59 -0600

    adding shared config

---
+++ config.go
@@ -0,0 +1,16 @@
+package config
+
+type Color string
+
+const (
+	ColorBlack  Color = "\u001b[30m"
+	ColorRed          = "\u001b[31m"
+	ColorGreen        = "\u001b[32m"
+	ColorYellow       = "\u001b[33m"
+	ColorBlue         = "\u001b[34m"
+	ColorReset        = "\u001b[0m"
+)
+
+func colorize(color Color, msg string) string {
+	return string(color) + msg + string(ColorReset)
+}
