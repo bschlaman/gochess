d627be6f73e01937836f61fa07fca58da156c48c
Brendan Schlaman <brendan.schlaman@gmail.com>
2021-02-01 12:56:52 -0600

    some more changes

---
+++ board.go
@@ -0,0 +1,15 @@
+package board
+
+import "fmt"
+
+func genLegalMoves(){
+}
+
+func makeMove(){
+}
+
+func undoMove(){
+}
+
+func eval(){
+}
--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func printBoard(){
+	fmt.Println(" ---- Game Board ---- ")
+}
+
 func main(){
+	printBoard()
 	fmt.Println("Chess!")
 }
